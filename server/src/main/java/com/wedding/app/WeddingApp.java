68a259393db9840afe3a6f20e6d7d90fabbd9f7f
manishachakraborty <manishachakraborty@192.168.0.6>
2018-03-18 19:10:06 +0530

    initial commit

---
+++ server/src/main/java/com/wedding/app/WeddingApp.java
@@ -0,0 +1,14 @@
+package com.wedding.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingApp {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WeddingApp.class, args);
+	}
+
+
+}
